6362cc64894401b5f888188fcbef9553ea5c7d72
fcosta00 <francescosta00@gmail.com>
2022-12-01 16:25:40 +0100

    fine lab con cross validation

--- Integrative/lez_22-12-01.R
+++ Integrative/lez_22-12-01.R
@@ -46,5 +46,28 @@ errLavaan(testData, mod_M3_fit)
 errLavaan(testData, mod_M3_1_fit)
 errLavaan(testData, mod_M3_2_fit)
 
+#CROSS VALIDATION
+nfold = 10
+folds = cut( seq(1,NROW(wisc)), breaks = nfold, labels = FALSE) 
 
+output = matrix(NA, nrow = nfold, ncol = 3)
 
+for (i in 1:nfold) {
+  indiciTest = which( folds == i)
+  
+  testData = wisc[indiciTest, ]
+  trainData = wisc[-indiciTest, ]
+  
+  mod_M3_fit = cfa(model = mod_M3_def, data = trainData)
+  mod_M3_1_fit = cfa(model = mod_M3_1_def, data = trainData)
+  mod_M3_2_fit = cfa(model = mod_M3_2_def, data = trainData)
+  
+  output[i, 1] = errLavaan(testData, mod_M3_fit)
+  output[i, 2] = errLavaan(testData, mod_M3_1_fit)
+  output[i, 3] = errLavaan(testData, mod_M3_2_fit)
+  
+}
+
+colnames(output) = c('err_mod', 'err_mod1', 'err_mod2')
+
+boxplot(output)
